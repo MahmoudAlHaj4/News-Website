52ca8602a9ce3d053dd707383776ae9e2b639f46
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2024-03-17 22:36:29 +0200

    add post news

--- backend/news.php
+++ backend/news.php
@@ -1,3 +1,16 @@
 <?php
 
 include("connection.php");
+
+$title = $_POST['title'];
+$content = $_POST['content'];
+
+
+$query = $mysqli->prepare('INSERT INTO content(title,content) values(?,?);');
+$query->bind_param('ss',$title,$content);
+$query->execute();
+
+
+$response["status"] = "success";
+$response["message"] = "news  created";
+echo json_encode($response);
